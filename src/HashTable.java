918cf9e6b50787f6e324dffcba469fa5488485bc
fguyomarch <Frederic.Guyomarch@univ-lille1.fr>
2018-09-21 11:24:43 +0200

    Initial ShareLaTeX Import

---
+++ src/HashTable.java
@@ -0,0 +1,7 @@
+public interface HashTable {
+    public put(String key, int value);
+    public int get(String key);
+    public boolean remove(String key);
+    public boolean contains(String key);
+    public int size();
+}
